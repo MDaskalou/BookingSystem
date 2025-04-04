4170170025f5edf44347ed9d7e1582d198034d55
Mikael Daskalou <mikael.daskalou@hotmail.com>
2025-04-04 08:06:04 +0200

    Fixat koden

--- BookingSystemSA.Infrastructure/obj/Debug/net8.0/BookingSystem.Infrastructure.AssemblyInfo.cs
+++ BookingSystemSA.Infrastructure/obj/Debug/net8.0/BookingSystem.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e6c2d78d6f1475e9a340c65411a4088d0a2396e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
